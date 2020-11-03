fe295f28902e37d074bc2b3f2eb513f73353547b
Jakub Solecki <jakub.solecki@onet.com.pl>
2020-11-03 20:14:02 +0100

    example project works

--- Fuzzy Logic/Volume-Control/src/main/java/pl/jakubsolecki/FuzzyController.java
+++ Fuzzy Logic/Volume-Control/src/main/java/pl/jakubsolecki/FuzzyController.java
@@ -1,34 +1,57 @@
-package pl.jsol;
+package pl.jakubsolecki;
 
 import net.sourceforge.jFuzzyLogic.FIS;
-import net.sourceforge.jFuzzyLogic.rule.FuzzyRuleSet;
+import net.sourceforge.jFuzzyLogic.FunctionBlock;
+import net.sourceforge.jFuzzyLogic.plot.JFuzzyChart;
+import net.sourceforge.jFuzzyLogic.rule.Variable;
 
 public class FuzzyController {
 
     public static void main(String[] args) {
         try {
             String fileName = args[0];
-            int intensityLevel = Integer.parseInt(args[1]);
+            int volumeLevel = Integer.parseInt(args[1]);
             int dayTime = Integer.parseInt(args[2]);
 
             FIS fis = FIS.load(fileName, true);
 
             if (fis == null) {
-                System.out.println("Cannot load file '" + fileName +"'");
+                System.err.println("Cannot load file '" + fileName +"'");
                 return;
             }
 
+            /* uses the newest version of jFuzzyLogic library (jFuzzyLogic.jar) */
+
+            FunctionBlock functionBlock = fis.getFunctionBlock(null);
+            JFuzzyChart.get().chart(functionBlock);
+
+            fis.setVariable("volumeLevel", volumeLevel);
+            fis.setVariable("dayTime", dayTime);
+
+            fis.evaluate();
+
+            Variable v = functionBlock.getVariable("volumeChange");
+            JFuzzyChart.get().chart(v, v.getDefuzzifier(), true);
+
+            System.out.println(fis);
+
+            /* for older version (jFuzzyLogic_1_2_1.jar) */
+/*
             FuzzyRuleSet fuzzyRuleSet = fis.getFuzzyRuleSet();
             fuzzyRuleSet.chart();
 
-            fuzzyRuleSet.setVariable("intensity_level", intensityLevel);
-            fuzzyRuleSet.setVariable("day_time", dayTime);
+            fuzzyRuleSet.setVariable("volumeLevel", volumeLevel);
+            fuzzyRuleSet.setVariable("dayTime", dayTime);
 
             fuzzyRuleSet.evaluate();
 
-            fuzzyRuleSet.getVariable("intensity_change").chartDefuzzifier(true);
+            fuzzyRuleSet.getVariable("volumeChange").chartDefuzzifier(true);
 
             System.out.println(fuzzyRuleSet);
+*/
+
+
+
         } catch (ArrayIndexOutOfBoundsException ex) {
             System.out.println("Incorrect number of parameters. Eg.: " +
                     "java Volume-Control string<file_fcl> int<intensity lvl> int<day time>");
